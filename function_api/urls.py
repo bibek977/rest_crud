bf9bf3a612061d0ad51ab67dd03adb31f4def57e
bibek977 <bhattaraibibek91@gmail.com>
2023-08-21 17:56:24 +0545

    class based views

--- function_api/urls.py
+++ function_api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',home,name='function_api'),
-    path('<int:pk>',data_api)
+    path('<int:pk>/',data_api),
+    path('',data_api),
+
+    # path('',DataAPI.as_view(),name='function_api'),
+    # path('<int:pk>/', DataAPI.as_view())
 ]
--- function_api/views.py
+++ function_api/views.py
@@ -5,11 +5,9 @@ from .models import *
 from .serializers import *
 from rest_framework import status
 
-@api_view()
-def home(request):
-        intern = Intern.objects.all()
-        serializer = InternSerializer(intern,many=True)
-        return Response(serializer.data)
+from rest_framework.views import APIView
+
+
 
 @api_view(['GET','POST',"PUT","PATCH","DELETE"])
 def data_api(request,pk=None):
@@ -55,6 +53,58 @@ def data_api(request,pk=None):
         return Response(s.errors)
     
     if request.method=="DELETE":
+        # id = request.data.get("id")
+        id = pk
+        i = Intern.objects.get(id=id)
+        i.delete()
+        return Response({'msg' : f'item {id} deleted'})
+
+
+class DataAPI(APIView):
+
+    def get(self,request,pk=None,format=None):
+        # id = request.data.get('id')
+        id = pk
+        if id is not None:
+            intern = Intern.objects.get(id=id)
+            serializer = InternSerializer(intern)
+            return Response(serializer.data)
+        intern = Intern.objects.all()
+        serializer = InternSerializer(intern,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request,format=None):
+        serializer = InternSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response ({'msg' : 'new data posted'}, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self,request,pk=None,format=None):
+        # id = request.data.get('id')
+        id = pk
+        i = Intern.objects.get(id=id)
+        s = InternSerializer(i, data=request.data)
+        if s.is_valid():
+            s.save()
+            return Response({'msg':f'data {id} updated completly'})
+
+        return Response(s.errors)
+    
+    def patch(self,request,pk=None,format=None):
+        # id = request.data.get('id')
+        id = pk
+        i = Intern.objects.get(id=id)
+        s = InternSerializer(i, data=request.data, partial=True)
+        if s.is_valid():
+            s.save()
+            return Response({'msg':f'data {id} updated partially'})
+
+        return Response(s.errors)
+    
+    def delete(self,request,pk=None,format=None):
+
         # id = request.data.get("id")
         id = pk
         i = Intern.objects.get(id=id)
