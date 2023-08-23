14a64b6e350ce1c90474a098227ceeb1f0f82c20
bibek977 <bhattaraibibek91@gmail.com>
2023-08-23 18:00:05 +0545

    custom authentication

--- token_auth/views.py
+++ token_auth/views.py
@@ -7,12 +7,18 @@ from .serializers import *
 from rest_framework.authentication import SessionAuthentication,TokenAuthentication
 from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
 
-
+from .customauth import CustomAuthentication
 
 class InternAPI(viewsets.ModelViewSet):
     queryset = Intern.objects.all()
     serializer_class = InternSerializer
 
-    authentication_classes = [TokenAuthentication]
+    # authentication_classes = [TokenAuthentication]
+    authentication_classes = [CustomAuthentication]
+
     # permission_classes = [IsAuthenticated]
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+    # IN url Section
+    # http://127.0.0.1:8000/token_auth/token_auth/?username=Apeal91
